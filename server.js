29c797571f3d86e191d5e6323c58ace2af8a62e0
Shardul23197 <shardul.dabhane@gmail.com>
2022-10-02 14:30:11 -0400

    Backend: Added environment variable and changed configuration

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const mainRoutes = require("./Routes/routes");
-const connectDB = require("./DB/connectDB");
-
-dotenv.config({ path: "config.env" });
+const users = require("./routes/routes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 
-app.use(mainRoutes);
 
-connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Your app is listening on http://localhost:${PORT}`);
-});
+// Connect to MongoDB
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
 
 // server static assets if in production
 if(process.env.NODE_ENV === 'production'){    
