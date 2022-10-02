cc8765a3d1699e46ebffe981e4bbea7d7fd5ff34
Mahajan, Rushali <rumahaj@iu.edu>
2022-10-02 19:53:33 -0400

    Initial frontend for home page and user dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,18 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Register from './components/Register';
+import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-    </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    // <Routes>
+    //   <Route path='/register' element={<Register/>}/>
+    //   <Route path='/dashboard' element={<Dashboard/>}/>
+    // </Routes>
+    // </BrowserRouter>
+    <Home></Home>
+    // <Dashboard></Dashboard>
   );
 }
 
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,166 @@
 import React from 'react'
+import '../App.css'; 
 
-const Dashboard = () => {
+export default function Dashboard() {
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
 
-export default Dashboard
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+  <div className="container-fluid">
+   
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars"></i>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    
+    <a className="navbar-brand" href="#">Fitocity</a>
+   
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="#">Dashboard</a>
+        </li>
+      </ul>
+  
+    </div>
+
+    <div className="d-flex align-items-center">
+
+    <div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Hello!
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Profile</a>
+    <a class="dropdown-item" href="#">Settings</a>
+    <a class="dropdown-item" href="#">Logout</a>
+  </div>
+</div>
+        
+      </div>
+    </div>
+    
+
+ 
+</nav>
+
+
+
+
+
+
+
+<div>
+
+<div class="cards-list">
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://img.freepik.com/free-photo/woman-doing-yoga-cleaning-chakra_23-2149276019.jpg?w=2000" />
+  </div>
+  <div class="card_title">
+    <p>Yoga</p>
+  </div>
+</div>
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://media.istockphoto.com/photos/dance-fitness-picture-id1067009516?k=20&m=1067009516&s=612x612&w=0&h=yQnFT71CeAq8R3QG4hlv4IyLLKnfwl28lMXy9xSn8sk=" />
+  </div>
+  <div class="card_title">
+    <p>Zumba</p>
+  </div>
+</div>
+
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://media.istockphoto.com/photos/attractive-sporty-girls-in-bodysuits-training-at-aerobics-workout-on-picture-id1064119338?k=20&m=1064119338&s=612x612&w=0&h=osPNv5SEc-mZvOisVdhvWGk2dqK-l5lYGIxi_WlAfZ4=" />
+  </div>
+  <div class="card_title">
+    <p>Aerobics</p>
+  </div>
+</div>
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://media.istockphoto.com/photos/workout-concept-sporty-african-american-woman-doing-abs-exercise-with-picture-id1322878383?k=20&m=1322878383&s=612x612&w=0&h=efco7G7L5NxOm956w6YApf_mYmXBGDEYVVyjsqgl9nI=" />
+  </div>
+  <div class="card_title">
+    <p>Core</p>
+  </div>
+</div>
+  
+  
+
+</div>
+</div>
+
+
+<div>
+    
+
+<div class="cards-list">
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://media.istockphoto.com/photos/indian-food-curry-butter-chicken-palak-paneer-chiken-tikka-biryani-picture-id1127563435?b=1&k=20&m=1127563435&s=612x612&w=0&h=eILdqLWa1ilkJm5qCq7s3HOnPuFea99CxYB5HxDbbVs=" />
+  </div>
+  <div class="card_title">
+    <p>Vegetarian</p>
+  </div>
+</div>
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://media.istockphoto.com/photos/indian-chicken-curry-picture-id471614507?k=20&m=471614507&s=612x612&w=0&h=snprycYKdTlsTn9vDNuFkWVPv-mwwRJoz2UidfhKvwQ=" />
+  </div>
+  <div class="card_title">
+    <p>Non Vegetarian</p>
+  </div>
+</div>
+
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://media.istockphoto.com/photos/healhty-vegan-lunch-bowl-avocado-quinoa-sweet-potato-tomato-spinach-picture-id893716434?k=20&m=893716434&s=612x612&w=0&h=wSf5StzaDtfpRhzdnUlQFhslcDgwLoQFC_ARycIVRwI=" />
+  </div>
+  <div class="card_title">
+    <p>Vegan</p>
+  </div>
+</div>
+
+<div class="card 3">
+  <div class="card_image">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDwGz4ybAQjO2v5xKZy4FMdej4RKCYZisvUNUljXBc9F7pdc97RLm_VbYAyORe6mBDX5g&usqp=CAU" />
+  </div>
+  <div class="card_title">
+    <p>Sea Food</p>
+  </div>
+</div>
+  
+  
+
+</div>
+</div>
+
+
+
+
+
+
+
+    </>
+  )
+  
+}
