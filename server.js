c5a591c444b88ab6083e09082c2ff50a72e02341
Shardul23197 <shardul.dabhane@gmail.com>
2022-10-02 12:08:34 -0400

    make it better

--- server.js
+++ server.js
@@ -21,3 +21,13 @@ connectDB();
 app.listen(PORT, () => {
     console.log(`Your app is listening on http://localhost:${PORT}`);
 });
+
+// server static assets if in production
+if(process.env.NODE_ENV === 'production'){    
+    app.use(express.static('frontend/build'))  // set static folder 
+    //returning frontend for any route other than api 
+    app.get('*',(req,res)=>{     
+        res.sendFile (path.resolve(__dirname,'frontend','build',         
+                      'index.html' ));    
+    });
+}
