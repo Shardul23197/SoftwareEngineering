fa3879fd31828350ac419d743996db85a26eb7cd
Herthoge, Dylan M <dherthog@iu.edu>
2022-10-11 17:11:03 -0400

    testing proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,63 @@
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import axios from 'axios';
 import './App.css';
 import React, { Component } from 'react';
 import Dashboard from './components/Dashboard';
 import Register from './components/Register';
 import Home from './components/Home';
 import Login from './components/Login';
+import { useState, useEffect } from 'react';
 
-class App extends Component {
-  state = {
-    data: null
-  };
+function App() {
+  const [auth, setAuth] = useState(null);
 
-  render() {
+  useEffect(() => {
+    axios.get('/auth/current-session').then(({data}) => {
+      setAuth(data);
+    })
+  }, [])
+  
+  if (auth === null) {
     return (
       <BrowserRouter>
       <Routes>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login/>}/>
+        <Route from='/*' element={<Home />}/>
+      </Routes>
+      </BrowserRouter>
+    );
+  }
+  if (auth) {
+    return (
+      <BrowserRouter>
+      <Routes>
+        {/* <Route path='/register' element={<Register/>}/> */}
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        {/* <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login/>}/> */}
+        <Route from='/*' element={<Dashboard />}/>
       </Routes>
       </BrowserRouter>
     );
   }
 
   
+  // return <Login />;
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
