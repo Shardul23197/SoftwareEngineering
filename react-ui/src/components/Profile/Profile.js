ea574321dd01891eb1d12fb90f5b92f4e8786e7b
Yenuga, Thathva Sri Sai Reddy <tyenuga@iu.edu>
2022-10-30 22:03:40 -0400

    Trainer dashboard revamped

--- react-ui/src/components/Profile/Profile.js
+++ react-ui/src/components/Profile/Profile.js
@@ -11,17 +11,31 @@ import {
   MDBBtn,
   MDBInput,
   MDBFile,
-  MDBTextArea
+  MDBTypography,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+  MDBTextArea,
 } from 'mdb-react-ui-kit';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import VideoCard from './VideoCard';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import './Profile.css'
+import { LinearProgress } from '@material-ui/core';
 
 import downloadFromAppStoreSVG from '../../images/app-store-images/Black_lockup/SVG/Download_on_the_App_Store_Badge.svg'
 
 export default function Profile() {
   let mfaRequired = localStorage.getItem('mfaRequired');
   const selector = useSelector(state => state.email)
+  const role = useSelector(state => state.role)
   const [userEmail, setUserEmail] = useState('')
   const [userFullName, setUserFullName] = useState('')
   const [userPhone, setUserPhone] = useState('')
@@ -30,8 +44,14 @@ export default function Profile() {
   const [mfaQrCodeUrl, setMfaQrCodeUrl] = useState('')
   const [mfaSecret, setMfaSecret] = useState('')
   const [dataFromState, setDataFromState] = useState(selector)
+  const [dataFromStateRole, setDataFromStateRole] = useState(role)
   const [trainerDetails, setTrainerDetails] = useState('')
   const [status, setStatus] = useState('todo')
+  const [videos, setVideos] = useState([])
+  const [varyingModal, setVaryingModal] = useState(false);
+  const [videoTitle, setVideoTitle] = useState('')
+  const [varyingUpload, setVaryingUpload] = useState(false)
+  const [hidden, setHidden] = useState(true)
 
   // Auth token and refresh token state
   const existingAuthtoken = localStorage.getItem('authToken') || '';
@@ -41,6 +61,7 @@ export default function Profile() {
   //get user data
   useEffect(() => {
     setDataFromState(selector)
+    setDataFromStateRole(role)
     axios.get('/api/users/profile/getdetails', { params: { email: dataFromState } })
       .then((res) => {
         setUserEmail(res.data.data.email)
@@ -59,6 +80,7 @@ export default function Profile() {
           console.log(error.response.data);
       })
 
+
     axios.get('/api/trainer/approvals', { params: { email: dataFromState } })
       .then((res) => {
         setStatus(res.data.status)
@@ -66,6 +88,16 @@ export default function Profile() {
         setStatus('notfound')
         console.log(error)
       })
+    
+      if(dataFromStateRole === 'trainer') {
+      axios.get('/api/trainer/videos', { params: { email: dataFromState } })
+      .then((res) => {
+        setVideos(res.data.data)
+      }).catch((error) => {
+        setVideos('')
+        console.log(error)
+      })
+    }
 
     // If the user has mfa enabled, get the google authenticator qr code url from the server
     if (mfaRequired === 'true') {
@@ -87,7 +119,9 @@ export default function Profile() {
         console.error(error);
       });
     }
-  }, [authToken, dataFromState, mfaRequired, selector, setMfaQrCodeUrl])
+  }, [authToken, dataFromState, mfaRequired, selector, setMfaQrCodeUrl, dataFromStateRole, role])
+
+
 
   const updateProfile = (event) => {
     event.preventDefault()
@@ -102,6 +136,10 @@ export default function Profile() {
     }).catch((err) => {
       toast('Something went wrong!')
     })
+  }
+
+  const onVideoTitleChange = (event) => {
+    setVideoTitle(event.target.value)
   }
 
   const onNameChange = (event) => {
@@ -121,6 +159,7 @@ export default function Profile() {
   }
 
   const updateImage = (event) => {
+    event.preventDefault()
     var formData = new FormData();
     var imagefile = document.getElementById('customFile');
     formData.append("image", imagefile.files[0]);
@@ -128,21 +167,30 @@ export default function Profile() {
     axios.post('/api/users/profile/upload', formData).then((res) => {
       toast('Profile Updated!')
       setUserImage(res.data.data)
+      setVaryingUpload(false)
     }).catch((err) => {
       toast('Something went wrong!')
     })
   }
 
   const uploadVideo = (event) => {
+    event.preventDefault()
+    setHidden(false)
     var formData = new FormData();
     var video = document.getElementById('video');
     formData.append("video", video.files[0]);
     formData.append('email', userEmail)
+    formData.append('title', videoTitle)
     axios.post('/api/trainer/upload', formData).then((res) => {
-      toast('Profile Updated!')
-      //setUserImage(res.data.data)
+      toast('Video Uploaded!')
+      setHidden(true)
+      setVideoTitle('')
+      videos.push(res.data)
+      setVaryingModal(false)
     }).catch((err) => {
       toast('Something went wrong!')
+      setHidden(true)
+      setVideoTitle('')
     })
   }
 
@@ -188,114 +236,189 @@ export default function Profile() {
     });
   }
 
-  return (
-    <section style={{ backgroundColor: '#eee' }}>
+  const renderByStatus = () => {
+    if (role === 'trainer') {
+      if (status === 'inprocess') {
+        return (<MDBCol style={{ 'width': '100%' }} md="6">
+          <br />
+          <p className="lead fw-normal mb-1">Tell us about yourself</p>
+          <form onSubmit={submitForApproval}>
+            <MDBTextArea value={trainerDetails} onChange={tellUsMore} id='textAreaExample' rows={10} />
+            <MDBBtn style={{ 'margin-top': '10px' }} >Submit</MDBBtn>
+          </form>
+        </MDBCol>)
+      }
+      else if (status === 'pending') {
+        return (<h1>Your profile is under Review</h1>)
+      }
+    }
+  }
 
+  return (
+    <div className="gradient-custom-2" style={{ backgroundColor: '#9de2ff' }}>
+        <LinearProgress id='spinner' hidden={hidden} color="secondary" />
       <ToastContainer />
       <form onSubmit={updateProfile}>
-        <MDBContainer className="py-5">
-          <MDBRow>
-            <MDBCol lg="4">
-              <MDBCard className="mb-4">
-                <MDBCardBody className="text-center">
-                  <MDBCardImage
-                    src={userImage}
-                    alt="avatar"
-                    className="rounded-circle"
-                    style={{ width: '150px' }}
-                    fluid />
-                  <p className="text-muted mb-1">{userFullName}</p>
-                  <MDBFile size='sm' id='customFile' onChange={updateImage} />
-                  <br />
-                  <div className="d-flex justify-content-center mb-2">
-                    <MDBBtn>Follow</MDBBtn>
-                    <MDBBtn outline className="ms-1">Message</MDBBtn>
+        <MDBContainer className="py-5 h-100">
+          <MDBRow className="justify-content-center align-items-center h-100">
+            <MDBCol lg="9" xl="7">
+              <MDBCard>
+                <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
+                  <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
+                    <div className="image-container">
+                    <MDBCardImage src={userImage}
+                      alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
+                      <div class="overlay">
+                        <Button class="icon" onClick={() => {
+                          setVaryingUpload(!varyingUpload);
+                        }}>
+                          <CloudUploadIcon className='fa user' />
+                        </Button>
+                      </div>
+                      </div>
                   </div>
+                  <div className="ms-3" style={{ marginTop: '130px' }}>
+                    <MDBTypography tag="h5">{userFullName}</MDBTypography>
+                    <MDBCardText>{userCity}</MDBCardText>
+                  </div>
+                </div>
+                <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
+                  <div className="d-flex justify-content-end text-center py-1">
+                    <div>
+                      {status === 'approved' && role === 'trainer' ? <Button variant="contained" color="default"
+                        className='material-button'
+                        startIcon={<CloudUploadIcon />}
+                        onClick={() => {
+                          setVaryingModal(!varyingModal);
+                        }}
+                      >Upload</Button> : ''}
+                    </div>
+                  </div>
+                </div>
+                <MDBCardBody className="text-black p-4">
+                  <div className="mb-5">
+                    <p className="lead fw-normal mb-1">About</p>
+                    <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
+                      <MDBRow>
+                        <MDBCol sm="3">
+                          <MDBCardText>Full Name</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="9">
+                          <MDBCardText className="text-muted">
+                            <MDBInput label='Name' onChange={onNameChange} value={userFullName}
+                              type='text' />
+                          </MDBCardText>
+                        </MDBCol>
+                      </MDBRow>
+                      <hr />
+                      <MDBRow>
+                        <MDBCol sm="3">
+                          <MDBCardText>Email</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="9">
+                          <MDBCardText className="text-muted">
+                            <MDBInput label='Email' onChange={onEmailChange} value={userEmail}
+                              type='text' />
+                          </MDBCardText>
+                        </MDBCol>
+                      </MDBRow>
+                      <hr />
+                      <MDBRow>
+                        <MDBCol sm="3">
+                          <MDBCardText>Phone</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="9">
+                          <MDBCardText className="text-muted">
+                            <MDBInput label='Phone' onChange={onPhoneChange} value={userPhone}
+                              type='text' />
+                          </MDBCardText>
+                        </MDBCol>
+                      </MDBRow>
+                      <hr />
+                      <MDBRow>
+                        <MDBCol sm="3">
+                          <MDBCardText>City</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="9">
+                          <MDBCardText className="text-muted">
+                            <MDBInput label='City' onChange={onCityChange} value={userCity}
+                              type='text' />
+                          </MDBCardText>
+                        </MDBCol>
+                      </MDBRow>
+                    </div>
+                    <MDBBtn style={{ 'marginTop': '10px' }}>Update</MDBBtn>
+                  </div>
+                  {renderByStatus()}
+                  <MDBRow md='4'>
+                    {status === 'approved' && role === 'trainer' && videos ? <VideoCard videos={videos} /> : ''}
+                  </MDBRow>
                 </MDBCardBody>
               </MDBCard>
-            </MDBCol>
-            <MDBCol lg="8">
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Full Name</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">
-                        <MDBInput label='Name' onChange={onNameChange} value={userFullName}
-                          type='text' />
-                      </MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Email</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">
-                        <MDBInput label='Email' onChange={onEmailChange} value={userEmail}
-                          type='text' />
-                      </MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Phone</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">
-                        <MDBInput label='Phone' onChange={onPhoneChange} value={userPhone}
-                          type='text' />
-                      </MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>City</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">
-                        <MDBInput label='City' onChange={onCityChange} value={userCity}
-                          type='text' />
-                      </MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBCardBody>
-              </MDBCard>
-              <MDBBtn>Update</MDBBtn>
-              <br />
             </MDBCol>
           </MDBRow>
         </MDBContainer>
       </form>
-
-      <MDBContainer className="py-5">
-        <MDBCol lg="8">
-          <MDBCard className="mb-4">
-            <MDBCardBody>
-              <MDBRow>
-                {status !== 'inprocess'?
-                  <MDBCol style={{ 'width': '100%' }} md="6">
-                    <br />
-                    <h1 style={{ 'margin': '20px' }} >Tell us more about yourself</h1>
-                    <form onSubmit={submitForApproval}>
-                      <MDBTextArea value={trainerDetails} onChange={tellUsMore} id='textAreaExample' rows={10} />
-                      <MDBBtn style={{ 'margin-top': '10px' }} >Submit</MDBBtn>
-                    </form>
-                  </MDBCol>
-                  : <h1>Your profile is under Review</h1>}
-                  <div>
-                  <MDBFile size='sm' id='video' onChange={uploadVideo} />
-                  </div>
-              </MDBRow>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBContainer>
+      <MDBModal show={varyingModal} setShow={setVaryingModal} tabIndex='-1'>
+        <MDBModalDialog>
+          <MDBModalContent>
+            <form onSubmit={uploadVideo}>
+              <MDBModalHeader>
+                <MDBModalTitle>Upload Video</MDBModalTitle>
+                <MDBBtn className='btn-close' color='none' onClick={() => setVaryingModal(!varyingModal)}></MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody>
+                <div className='mb-3'>
+                  {varyingModal && (
+                    <MDBInput
+                      value={videoTitle}
+                      onChange={onVideoTitleChange}
+                      labelClass='col-form-label'
+                      label='Title of your video:'
+                    />
+                  )}
+                </div>
+                <div className='mb-3'>
+                  {varyingModal && (
+                    <MDBFile size='sm' id='video' />
+                  )}
+                </div>
+              </MDBModalBody>
+              <MDBModalFooter>
+                <MDBBtn color='secondary' onClick={() => setVaryingModal(!varyingModal)}>
+                  Close
+                </MDBBtn>
+                <MDBBtn>Upload</MDBBtn>
+              </MDBModalFooter>
+            </form>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+      <MDBModal show={varyingUpload} setShow={setVaryingUpload} tabIndex='-1'>
+        <MDBModalDialog>
+          <MDBModalContent>
+            <form onSubmit={updateImage}>
+              <MDBModalHeader>
+                <MDBModalTitle>Upload Profile Picture</MDBModalTitle>
+                <MDBBtn className='btn-close' color='none' onClick={() => setVaryingUpload(!varyingUpload)}></MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody>
+                <div className='mb-3'>
+                  {varyingUpload && (
+                    <MDBFile size='sm' id='customFile' />
+                  )}
+                </div>
+              </MDBModalBody>
+              <MDBModalFooter>
+                <MDBBtn color='secondary' onClick={() => setVaryingUpload(!varyingUpload)}>
+                  Close
+                </MDBBtn>
+                <MDBBtn>Upload</MDBBtn>
+              </MDBModalFooter>
+            </form>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
       <MDBContainer className="py-5">
         <MDBCol lg="12">
           <MDBCard className="mb-4">
@@ -356,6 +479,6 @@ export default function Profile() {
           </MDBCard>
         </MDBCol>
       </MDBContainer>
-    </section>
+    </div>
   );
 }
