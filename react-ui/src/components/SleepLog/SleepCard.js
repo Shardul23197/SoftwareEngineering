a3971495fa244fe8e17eec208d6684ff27c3cb78
Herthoge, Dylan M <dherthog@iu.edu>
2022-11-14 13:11:07 -0500

    updated formatting

--- react-ui/src/components/SleepLog/SleepCard.js
+++ react-ui/src/components/SleepLog/SleepCard.js
@@ -3,27 +3,39 @@ import Card from 'react-bootstrap/Card';
 
 const SleepCard = ({ sleeps }) => {
 
+    const formatDate = (date) => {
+      const dateAmOrPm = date.getHours() / 12 === 1 ? 'PM' : 'AM';
+
+      let dateHour = date.getHours();
+      if (dateHour === 0) dateHour = 12;
+      else if (dateHour > 12) dateHour -= - 12;
+
+      let dateSecond = date.getSeconds().toString();
+      if (dateSecond.length === 1) dateSecond = '0' + dateSecond;
+
+      let dateMinute = date.getMinutes().toString();
+      if (dateMinute.length === 1) dateMinute = '0' + dateMinute;
+
+      return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}  ${dateHour}:${dateMinute}:${dateSecond} ${dateAmOrPm}`;
+    }
+
     return (
         sleeps.map((sleep) => {
-            const startDate = new Date(sleep.startDate);
-            const startDateAmOrPm = startDate.getHours() / 12 === 1 ? 'PM' : 'AM';
-            const endDate = new Date(sleep.endDate);
-            const endDateAmOrPm = startDate.getHours() / 12 === 1 ? 'PM' : 'AM';
-            let startDateHour = startDate.getHours();
-            if (startDate.getHours() === 0) startDateHour = 12;
-            else if (startDate.getHours() > 12) startDateHour = startDate.getHours() - 12;
+            const startDateStr = formatDate(new Date(sleep.startDate));            
+            const endDateStr = formatDate(new Date(sleep.endDate));
+            
             return (
                 <Card style={{ width: '18rem', 'marginRight': '6%', 'marginBottom': '10%' }}>
                     <Card.Body>
                         <Card.Title>{sleep.title}</Card.Title>
-                        <Card.Subtitle style={{ 'marginBottom': '10px'}}>{new Date(sleep.date).toDateString()}</Card.Subtitle>
+                        <Card.Subtitle style={{ 'marginBottom': '10px'}}>{new Date(sleep.date).toDateString().replace(' ', ', ')}</Card.Subtitle>
                         <hr style={{ 'marginTop': '5px', 'marginBottom': '5px' }}/>
                         <Card.Text style={{ 'marginBottom': '5px'}}>
-                            Start:  {`${startDate.getDate()}/${startDate.getMonth()}/${startDate.getFullYear()}  ${startDateHour}:${startDate.getMinutes()}:${startDate.getSeconds()} ${startDateAmOrPm}`}
+                            Start:  {startDateStr}
                         </Card.Text>
                         <hr style={{ 'marginTop': '5px', 'marginBottom': '5px' }}/>
                         <Card.Text style={{ 'marginBottom': '5px'}}>
-                            End:  {`${endDate.getDate()}/${endDate.getMonth()}/${endDate.getFullYear()}  ${endDate.getHours()}:${endDate.getMinutes()}:${endDate.getSeconds()} ${endDateAmOrPm}`}
+                            End:  {endDateStr}
                         </Card.Text>
                         <hr style={{ 'marginTop': '5px', 'marginBottom': '5px' }}/>
                         <Card.Text>Comments</Card.Text>
